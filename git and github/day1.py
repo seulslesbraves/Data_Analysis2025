df279d3eea6660f1d27cbb6508dae9dfae93d0ff
dangolrbna0623@gmail.com <dangolrbna0823@gmail.com>
2025-10-30 08:24:30 +0545

    forth commit

--- git and github/day1.py
+++ git and github/day1.py
@@ -11,8 +11,9 @@ An online platform to store our project code."""
 # U stands for UNtracked...  # A stands for added in git   
 # M stands for M
 3. Add files to github : git add {file_name}}  (to add all files we use git add . )
-4. Commit the files : git commit ~m "Message"
+4. Commit the files : git commit -m "Message"
 5. Push the git to github : git push -u origin {branch_name} // git push origin {branch_name}
 # -u : Upstream that saves the repository
 # At first push, it is recommended to use : git push -u origin {branch_name} 
+6. To pull the code from github : git pull origin {branch_name}
 """
